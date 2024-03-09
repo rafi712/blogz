3b2b5412d97046e8b3f460329ec3e8f163f0a965
rafi712 <rafiakmal712@gmail.com>
2024-03-09 14:07:29 +0700

    create migrations

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Category;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'test@example.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        Category::factory()->create([
+            'name' => 'Uncategorized',
+        ]);
+    }
+}
